8c563b77468e168de49e06d6fac2f73784faa6d8
Helena Upshaw <helenaupshaw@helenas-air.lan>
2021-07-26 19:07:16 -0400

    JS array & replacement edits

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ function randomValueFromArray(array){
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-let insertX = "Willy the Goblin, Big Daddy, Father Christmas";
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 
-let insertY = "the soup kitchen, Disneyland, the White House";
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 
-let insertZ = "spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away";
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
@@ -25,20 +25,21 @@ function result() {
   let yItem = randomValueFromArray(insertY);
   let zItem = randomValueFromArray(insertZ);
 
-  newStory.replace(":insertx:", xItem);
-  newStory.replace(":inserty:", yItem);
-  newStory.replace(":insertz:", zItem);
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+  newStory = newStory.replace(':insertx:', xItem);
 
   if(customName.value !== '') {
     let name = customName.value;
-    newStory.replace("Bob", name);
+    newStory = newStory.replace('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
     let weight = Math.round(300/14) + ' stone';
     let temperature =  Math.round((94-32) * 5/9) + ' centigrade';
-    newStory.replace('300 pounds', weight);
-    newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
+    newStory = newStory.replace('94 fahrenheit', temperature);
 
   }
 
